4a6f7f82f804a6b438ec69449c65e050cd4d657d
Oussama-Zoghlami <oussamazoghlami62@gmail.com>
2024-01-03 00:07:16 +0100

    with corsConfig

--- src/main/java/com/comeback/securityauthback/SecuritybackendApplication.java
+++ src/main/java/com/comeback/securityauthback/SecuritybackendApplication.java
@@ -7,8 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@EnableScheduling
+@EnableAspectJAutoProxy
 @SpringBootApplication(scanBasePackages = "com.comeback.securityauthback")
+
+@CrossOrigin(origins = "http://localhost:4200/")
 
 
 public class SecuritybackendApplication implements CommandLineRunner {
@@ -19,10 +33,10 @@ public class SecuritybackendApplication implements CommandLineRunner {
 		SpringApplication.run(SecuritybackendApplication.class, args);
 	}
 
-	public void run(String... args){
+	public void run(String... args) {
 		User adminAccount = userRepository.findByRole(Role.ADMIN);
-		if(null == adminAccount){
-			User user =new User();
+		if (null == adminAccount) {
+			User user = new User();
 
 			user.setEmail("admin@gmail.com");
 			user.setFirstname("admin");
@@ -32,7 +46,21 @@ public class SecuritybackendApplication implements CommandLineRunner {
 			userRepository.save(user);
 
 		}
-
+	}
+		@Bean
+		public CorsFilter corsFilter() {
+			CorsConfiguration corsConfiguration = new CorsConfiguration();
+			corsConfiguration.setAllowCredentials(true);
+			corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+			corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+					"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+					"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+			corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+					"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+			corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+			UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+			urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+			return new CorsFilter(urlBasedCorsConfigurationSource);
+		}
 	}
 
-}
--- src/main/java/com/comeback/securityauthback/controller/AuthenticationController.java
+++ src/main/java/com/comeback/securityauthback/controller/AuthenticationController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+@CrossOrigin(origins="*")
 
 @RestController
 @RequestMapping("/api/v1/auth")
